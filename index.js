bc910cc37eb7798cec3eed6eea28a0daa7650342
Dawn Isabel <dawn.isabel@gmail.com>
2020-09-28 13:29:07 -0400

    Throw errors so API use can catch them (#110)

--- index.js
+++ index.js
@@ -101,9 +101,10 @@ class Applesign {
       await this.zipIPA();
     } catch (e) {
       process.exitCode = 1;
-      console.error(e);
+      throw e;
+    } finally {
+      await this.cleanup();
     }
-    await this.cleanup();
     return this;
   }
 
